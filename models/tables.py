344e70f105327033ed6aaee7a96ff6da0f012825
nailsonseat <aadarshv3@gmail.com>
2023-07-23 21:49:23 +0530

    all tables are now defined under tables.py

--- models/tables.py
+++ models/tables.py
@@ -33,3 +33,13 @@ class WishlistItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     product_id = db.Column(db.Integer, nullable=False)
+
+
+class Product(db.Model):
+    __bind_key__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    section = db.Column(db.String(50), nullable=False)
+    manufacture_date = db.Column(db.Date)
+    expiry_date = db.Column(db.Date)
+    rate_per_unit = db.Column(db.Float, nullable=False)
