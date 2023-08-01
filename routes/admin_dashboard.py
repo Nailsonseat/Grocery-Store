5ff9cb03961ebbbf13a417c4db4dea8e195c20d7
nailsonseat <aadarshv3@gmail.com>
2023-08-01 15:35:30 +0530

    removed redundant code

--- routes/admin_dashboard.py
+++ routes/admin_dashboard.py
@@ -6,5 +6,4 @@ admin_dashboard_bp = Blueprint('admin_dashboard', __name__)
 
 @admin_dashboard_bp.route('/admin/dashboard')
 def admin_dashboard():
-    # Add code to display the admin dashboard here
     return render_template('admin_dashboard.html')
