bdcee84cce18b11a91c3bcb2624c04a9e9369c16
nailsonseat <aadarshv3@gmail.com>
2023-08-02 15:38:01 +0530

    added route for category page

---
+++ routes/categories.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from models.tables import db, Category, Product
+import os
+
+categories_bp = Blueprint('categories', __name__)
+
+
+@categories_bp.route('/categories', methods=['GET', 'POST'])
+def categories():
+    if request.method == 'POST':
+        new_category_name = request.form['category_name']
+        new_category_image = request.files['category_image']
+
+        if new_category_image:
+            image_extension = new_category_image.filename.split('.')[-1]
+            image_filename = f"{new_category_name}_category.{image_extension}"
+            new_category_image.save(os.path.join(
+                'static', 'category_images', image_filename))
+
+        else:
+            image_filename = 'placeholder.jpg'  # Use the placeholder image
+
+        # Create a new Category instance and add it to the database
+        new_category = Category(name=new_category_name,
+                                image_filename=image_filename)
+        db.session.add(new_category)
+        db.session.commit()
+
+        # Redirect back to the edit_categories page
+        return redirect(url_for('categories.categories'))
+
+    categories = Category.query.all()
+    return render_template('categories.html', categories=categories)
