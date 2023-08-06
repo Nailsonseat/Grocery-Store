1bf48a9c89f17b1d551b98bf460b7187bf8a3242
nailsonseat <aadarshv3@gmail.com>
2023-08-06 15:51:05 +0530

    added logic for filters

--- routes/home.py
+++ routes/home.py
@@ -32,4 +32,29 @@ def home(name):
         session['query'] = ''
         products = Product.query.all()
 
-    return render_template('home.html', products=products, username=username)
+    # Apply filters
+    filter_category = request.args.get('filter_category')
+    filter_max_price = request.args.get('filter_max_price')
+    filter_min_price = request.args.get('filter_min_price')
+    filter_under_mfd = request.args.get('filter_under_mfd')
+    filter_over_expired = request.args.get('filter_over_expired')
+
+    if filter_category:
+        products = [
+            product for product in products if product.category_id == int(filter_category)]
+
+    if filter_max_price:
+        products = [product for product in products if product.rate_per_unit <= float(
+            filter_max_price)]
+
+    if filter_min_price:
+        products = [product for product in products if product.rate_per_unit >= float(
+            filter_min_price)]
+
+    if filter_under_mfd:
+        products = [
+            product for product in products if product.manufacture_date and product.manufacture_date <= filter_under_mfd]
+
+    if filter_over_expired:
+        products = [
+            product for product in products if product.expiry_date and product.expiry_date > filter_over_expired]
