082340d4272d118dcdc62b4ec8a6393846da00d3
nailsonseat <aadarshv3@gmail.com>
2023-08-06 15:50:40 +0530

    defined home route

--- routes/home.py
+++ routes/home.py
@@ -1,13 +1,35 @@
-from flask import Blueprint, redirect, render_template
-from models.tables import db, Product
+from flask import Blueprint, redirect, render_template, session, request
+from models.tables import Product, Category, Cart
 
 
 home_bp = Blueprint('home', __name__)
 
 
-@home_bp.route('/<string:username>/home', methods=['GET', 'POST'])
-def home(username):
-    # Get all products from the "products" database
-    products = Product.query.all()
+def get_category(categories, id):
+    for i in categories:
+        if id == i.id:
+            return i
+
+
+@home_bp.route('/<string:name>/home', methods=['GET', 'POST'])
+def home(name):
+    categories = Category.query.all()
+    cart = Cart.query.all()
+
+    cart_map = {}
+
+    for i in cart:
+        cart_map[i.product_id] = i.quantity
+
+    category_map = {}
+
+    session['query'] = request.args.get('search_query')
+
+    if session['query']:
+        products = Product.query.filter(
+            Product.name.ilike(f'%{session["query"]}%')).all()
+    else:
+        session['query'] = ''
+        products = Product.query.all()
 
     return render_template('home.html', products=products, username=username)
