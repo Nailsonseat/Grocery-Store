7d9e4d581b81fe21ec7417476465ffc20469bc82
nailsonseat <aadarshv3@gmail.com>
2023-08-10 16:01:22 +0530

    logout logic added

--- routes/logout.py
+++ routes/logout.py
@@ -6,6 +6,8 @@ logout_bp = Blueprint('logout', __name__)
 
 @logout_bp.route('/logout')
 def logout():
-    session.pop('username', None)  # Remove the username from the session
+    session.pop('email', None)  # Remove the username from the session
+    session.pop('name', None)
+    session.pop('user_id', None)
     # Redirect to the home page or login page
     return redirect(url_for('login.login'))
