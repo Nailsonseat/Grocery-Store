5762a4b0da584dcd0d293b4fb6eea016c9472a03
nailsonseat <aadarshv3@gmail.com>
2023-08-13 16:39:00 +0530

    main logic implemented

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 # app.py
 
-from flask_sqlalchemy import SQLAlchemy
 from routes.login import login_bp
 from routes.register import register_bp
 from routes.admin_dashboard import admin_dashboard_bp
 from routes.home import home_bp
 from routes.logout import logout_bp
-from routes.edit_categories import categories_bp
-from routes.edit_products import products_bp
+from routes.categories import categories_bp
+from routes.products import products_bp
+from routes.profile import profile_bp
+from routes.my_orders import my_orders_bp
+from routes.cart import cart_bp
+from routes.coupons import coupons_bp
+from routes.pending_orders import pending_orders_bp
+from routes.distribute_coupons import distribute_coupons_bp
 from flask import Flask, render_template
 from models.tables import db, User
 
@@ -26,12 +31,14 @@ app.config['SQLALCHEMY_BINDS'] = {
 
 
 def create_initial_admin():
-    admin_username = 'admin'
+    admin_name = 'admin'
+    admin_email = 'admin@test.com'
     admin_password = 'adminpass'
+    admin_mobile_number = "1234567890"
 
-    admin_user = User.query.filter_by(username=admin_username).first()
+    admin_user = User.query.filter_by(email=admin_email).first()
     if not admin_user:
-        admin_user = User(username=admin_username,
+        admin_user = User(name=admin_name, email=admin_email, mobile_number=admin_mobile_number,
                           password=admin_password, is_admin=True)
         db.session.add(admin_user)
         db.session.commit()
@@ -54,6 +61,12 @@ app.register_blueprint(home_bp)
 app.register_blueprint(logout_bp)
 app.register_blueprint(categories_bp)
 app.register_blueprint(products_bp)
+app.register_blueprint(profile_bp)
+app.register_blueprint(cart_bp)
+app.register_blueprint(my_orders_bp)
+app.register_blueprint(coupons_bp)
+app.register_blueprint(distribute_coupons_bp)
+app.register_blueprint(pending_orders_bp)
 # ... (other routes and code)
 
 if __name__ == '__main__':
