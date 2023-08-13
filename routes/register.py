086ee3476fa9a7da4b45c9d615fe07001d77534e
nailsonseat <aadarshv3@gmail.com>
2023-08-13 16:24:08 +0530

    registration page logic improved

--- routes/register.py
+++ routes/register.py
@@ -1,6 +1,4 @@
-# routes/register.py
-
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, flash
 from models.tables import db, User
 
 register_bp = Blueprint('register', __name__)
@@ -9,23 +7,28 @@ register_bp = Blueprint('register', __name__)
 @register_bp.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        name = request.form['name']
+        email = request.form['email']
+        mobile_number = request.form['mobile_number']
+
         password = request.form['password']
         confirm_password = request.form['confirm_password']
 
-        # Check if the username is available
-        if User.query.filter_by(username=username).first() is None:
+        # Check if the email is available
+        if User.query.filter_by(email=email).first() is None:
             # Check if the password and confirm_password match
             if password == confirm_password:
                 # Create a new user record and add it to the database
-                new_user = User(username=username, password=password)
+                new_user = User(name=name, email=email,
+                                mobile_number=mobile_number, password=password)  # Include date_of_birth
                 db.session.add(new_user)
                 db.session.commit()
 
-                return "User registered successfully!"
+                flash('Registration complete', 'reg_success')
+                return redirect('/')
 
             return "Passwords do not match. Please try again."
 
-        return "Username already taken. Please choose a different one."
+        return "Email already taken. Please use a different one."
 
     return render_template('register.html')
