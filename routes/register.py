daf873788f63d8a063b241d2e912f30853387a67
nailsonseat <aadarshv3@gmail.com>
2023-08-13 22:11:16 +0530

    integrity checks for registration page

--- routes/register.py
+++ routes/register.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
 from models.tables import db, User, Coupon, UserCoupon
+import re
 
 register_bp = Blueprint('register', __name__)
+
+
+# Function to validate email format
+def is_valid_email(email):
+    email_pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    return email_pattern.match(email)
 
 
 @register_bp.route('/register', methods=['GET', 'POST'])
@@ -18,6 +25,24 @@ def register():
         if User.query.filter_by(email=email).first() is None:
             # Check if the password and confirm_password match
             if password == confirm_password:
+
+                # Perform additional integrity checks for name and password
+                if not name.strip():
+                    flash("Name cannot be empty.", "reg_error")
+                    return redirect(url_for('register.register'))
+
+                if not name.replace(" ", "").isalpha():
+                    flash("Name should only contain letters and spaces.", "reg_error")
+                    return redirect(url_for('register.register'))
+
+                if not is_valid_email(email):
+                    flash("Invalid email format.", "reg_error")
+                    return redirect(url_for('register.register'))
+
+                if len(password) < 8:
+                    flash("Password must be at least 8 characters long.", "reg_error")
+                    return redirect(url_for('register.register'))
+
                 # Create a new user record and add it to the database
                 new_user = User(name=name, email=email,
                                 mobile_number=mobile_number, password=password)  # Include date_of_birth
